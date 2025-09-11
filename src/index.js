a147cac33f0f65b7d2efd726138dad792ddf519e
Kailash Aghav <kailashaghav1354@gmail.com>
2025-09-11 17:46:47 +0530

    Api errors response and handlers initialized

--- src/index.js
+++ src/index.js
@@ -7,7 +7,20 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("EXPRESS APP ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDB connection Failed !!! `, err);
+  });
 
 /*
 import mongoose, { connect } from "mongoose";
