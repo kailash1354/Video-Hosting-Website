abd035a1345faa0833b7f65a82f42757c81b9f29
Kailash Aghav <kailashaghav1354@gmail.com>
2025-09-13 15:46:21 +0530

    adding userModel and videoModel

---
+++ src/models/video.model.js
@@ -0,0 +1,55 @@
+import mongoose, { Schema, Schema, Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const Schema = new Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, //cloudinary url
+            required: true
+        },
+        coverImage: {
+            type: String, //cloudinary url
+        },
+        watchHistory: [
+            {
+                type: mongoose.Schema.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+
+export const User = mongoose.model("User", userSchema);
